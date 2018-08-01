12c266e6e0ad9fb54afc286523cba99ea4b06e4c
DeepenD <deepenrajdawadi@gmail.com>
2018-08-01 11:33:24 -0400

    mvn hello world project for Jenkins

---
+++ src/main/java/veridic/App.java
@@ -0,0 +1,16 @@
+package veridic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		System.out.println( "======================================================================================" );
+        System.out.println( "Hello World!" );
+		System.out.println( "This is a test from Jenkins with MVN plugin!" );
+		System.out.println( "======================================================================================" );
+	}
+}
